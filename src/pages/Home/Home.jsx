eaa089581d2731970ee8cbc5730ebc03fa074a11
rifat1752 <rifathossain1752@gmail.com>
2024-09-06 22:29:48 +0600

    vercel deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
-import Categories from "./Categories/Categories.Jsx";
+
 import Rooms from "../../components/Rooms/Rooms";
 import { Helmet } from "react-helmet";
 import { useLoaderData } from "react-router-dom";
+import Categories from "./Categories/Categories.jsx";
 
 const Home = () => {
   const rooms = useLoaderData()
